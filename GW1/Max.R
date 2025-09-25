57121c9f1ea5501024c44e36ce9e9ca5f82fcdb2
mhmc0808 <123744321+mhmc0808@users.noreply.github.com>
2025-09-25 15:43:58 +0100

    Update Max.R

--- GW1/Max.R
+++ GW1/Max.R
@@ -1,14 +1,11 @@
 setwd("GW1") ## comment out of submitted
 a <- scan("shakespeare.txt",what="character",skip=83,nlines=196043-83,
           fileEncoding="UTF-8")
-a_full <- a
 
-#a <- a[1:50000]
-a
 
-# PREPROCESSING
+### PREPROCESSING
 
-## drop stage directions
+# drop stage directions
 stage_start = grep("[[]", a)
 
 stage_directions <- c()
@@ -22,14 +19,10 @@ for (i in stage_start){
   stage_directions <- append(stage_directions, c(range(i, j)))
 }
 # dropped stage directions
-a_1 <- a[-unique(stage_directions)]
-
-# make each punctuation its own word
+a <- a[-unique(stage_directions)]
 
 
-
-
-# drop names and numerals
+# Drop names and numbers
 
 uppers_nums <- c()
 for (i in 1:length(a_1)){
@@ -40,80 +33,46 @@ for (i in 1:length(a_1)){
     uppers_nums <- append(uppers_nums, i)
   }
   # If numeric
-  if (grepl("[0-9]", a_1[i])){
+  if (grepl("[0-9]", a[i])){
     uppers_nums <- append(uppers_nums, i)
   }
 }
 # remove names and numerals
-a_2 <- a_1[-uppers_nums]
+a <- a[-uppers_nums]
 
-# Also maybe remove all I's for when it says ACT I and SCENE I?
-#ACT_words <- which(a_1 == "ACT")
-#a_1[ACT_words+1]
+# get rid of _ and -
+a <- gsub("[_]", "", a)
+a <- gsub("[-]", "", a)
 
-# get rid of _ (chose not to get rid of - for now)
-a_2 <- gsub("[_]", "", a_2)
 
 # Ex 4: Make punctuation marks into their own words
-punctuation_marks <- c("!", ".", ":", ";", ",", "?")
+punctuation_marks <- c("!", "\\.", ":", ";", ",", "\\?")
 
-split_punct <- function(word_vector, punctuation) {
-  # input: word vector, punctuation mark list
-  # separates punctuation from words and returns a new vector where
-  # each punctuation mark is its own separate entry
+# Function that splits the punctuation into its own entry in our word vector
+split_punct <- function(word_vector, punctuation){
   
-  # we need to get punctuation list into correct formatting
-  punct <- paste0("[", paste(ifelse(punctuation == ".", "\\.", punctuation), collapse=""), "]")
-  
-  # identify which words in the vector contain punctuation
-  punct_indexes <- grep(punct, word_vector)
-  
-  # Initiate empty vector with maximum possible size (2x length in the case every word has punctuation)
-  max_length <- 2 * length(word_vector)
-  vec <- character(max_length)
-  
-  # initialise an index to track our position in the result vector
-  pos <- 1
-  
-  # looping through each word...
-  for (i in 1:length(word_vector)) {
-    
-    # Check if this word has punctuation at the end
-    if (i %in% punct_indexes) {
-      
-      # Step 5a: Extract the word part (without punctuation)
-      # Using regex backreference \\1 to get the first capture group
-      clean_k <- gsub(punct, "\\1", word_vector[i])
-      
-      # find the punctuation mark
-      k_string <- strsplit(word_vector[i], "")[[1]]
-      is_punct <- which(k_string %in% punctuation)
-      punct_k <- k_string[is_punct]
-    
-      # add the clean word and punctuation mark to the result vector
-      vec[pos] <- clean_k
-      vec[pos+1] <- punct_k
-      pos <- pos + 2
-      
-    } else {
-      # if no punctuation, add as is
-      vec[pos] <- word_vector[i]
-      pos <- pos + 1
-    }
+  # Separate all strings with punctuation by introducing a space between the word and the punctuation
+  for (p in punctuation){
+    p_space <- paste0(" ", paste(p))
+    #print(p_space)
+    word_vector <- gsub(p, p_space, word_vector)
+    #print(word_vector)
   }
+  #print(word_vector)
+  # We then remove 
+  word_vector <- unlist(strsplit(word_vector, split = " "))
   
-  # final result is vector minus empty spaces
-  result <- vec[1:(pos - 1)]
-  return(result)
+  return(word_vector)
+  
 }
 
-split_punct(c("a?", "b,", "c.", "d;", "e:", "f!"), punctuation_marks)
-system.time(a_3 <- split_punct(a_2, punctuation_marks))
+
+system.time(a <- split_punct(a, punctuation_marks))
+
 
 # make all lower case for simplicity
-a_4 <- tolower(a_3)
+a <- tolower(a)
 
-a_4
 
 # Ex 5: Matching
 
@@ -129,13 +88,14 @@ frequency_ranks <- rank(-word_frequencies, ties.method = "average") # check aver
 top_indices <- which(frequency_ranks <= 1000)
 # Extract the top 1000 words
 top_words <- unique_words[top_indices]
-top_words
 
+
+## TEXT SETUP
 
 # Ex 6
 
 # index of top words positions in text
-index_top_words <- match(a_4, top_words)
+index_top_words <- match(a, top_words)
 
 # choose mlag 
 mlag <- 4
@@ -145,46 +105,90 @@ n <- length(a_4)
 M <- matrix(nrow=n-mlag, ncol=mlag+1)
 # Put top words index into 1st colm of matrix, then three lags in subsequent colms
 for (m in 1:(mlag+1)){
-  M[,m] <- index_top_words[m:(n-5+m)]
+  M[,m] <- index_top_words[m:(n-(mlag+1)+m)]
 }
 
-M[,1]
+
+## SENTENCE GENERATION
 
 # Ex 7
 
-
+# Function that randomly generates the next word of a given key using the text
 next.word <- function(key, M, M1, w=rep(1,ncol(M)-1)){
   # key - word sequence for which the next word is generated
   # M is matrix M with top word indexed and lags
   # M1 is the indexing vector of the entire text (index_vector)
   # w - vector of mixture weights
   key_length <- length(key)
-  mc <- mlag - length(key) + 1
+  mc <- mlag - key_length +1
+  # initialise next_word as NA
+  next_word <- NA
   
+  # finds the rows of M where the text matches the key
   ii <- colSums(!(t(M[,mc:mlag,drop=FALSE])==key))
-  print(ii)
-  
-  # matching rows, where key matches rows of M
-  matching_rows <- integer(0)
-  for (r in 1:nrow(M)) {
-    if (all(M[r, mc:mlag] == key, na.rm=TRUE)) {  # Compare the appropriate columns
-      matching_rows <- c(matching_rows, r)
-    }
+  match_rows <- which(ii == 0 & is.finite(ii))
+  print(match_rows)
+
+  # now randomly selects the next word following the key in the text
+  # (if NA, chooses again)
+  while (is.na(next_word)){
+  random_row <- sample(match_rows, 1)
+  next_word <- M[random_row,mlag+1]
   }
-  print(matching_rows)
+  return(next_word)
+}
+
+# Ex 8 + 9
+
+# Function that iteratively produces sentence using next.word function
+sim_shakespeare <- function(key_length, M, M1, top_words){
   
-  #for (r in nrow(M)){
-  #  if (key == M[r,(1:length(key))]){
-  #    
-  #  }
+  # Choose first word
+  # first, take top_words where punctuation is not included
+  punctuation <- c(",", ".", ";", ":", "!", "?")
+  tw_no_punct <- top_words[!top_words %in% punctuation]
+  
+  # initialise random starter token
+  random_starter_token <- sample(tw_no_punct, 1)
+  key <- which(top_words == random_starter_token)
+  key_sentence <- key
+  
+  # find index of period for ending our while loop
+  period_index <- (which(top_words=="."))
+  while (key_sentence[length(key_sentence)] != period_index){
+    
+    print(key)
+    
+    
+    # Take next word using next.word function
+    next_word <- next.word(key, M, M1)
+    # append next_word to sentence
+    key_sentence <- append(key_sentence, next_word)
+    
+    # if the sentence is shorter than the desired key_length, just make sentence the key
+    if (length(key_sentence) < key_length){
+      key <- key_sentence
+    }else{
+      # take last key length no. of words of sentence
+      key <- key_sentence[(length(key_sentence)-key_length+1):length(key_sentence)]
+    }
+    # collapse sentence
+    sentence <- paste(top_words[key_sentence], collapse=" ")
+    print(sentence)
+  }
+  return(sentence)
 }
 
 
+# Let's simulate Shakespeare
+sim_shakespeare(2, M, M1, top_words)
 
 
-ncol(M)
 
-key = c("bare")
 
-next.word(key,M,M1,w=rep(1,ncol(M)-1))
+
+# the O's (not mentioned as a special case)
+# ACT I - should we remove this separately for more complete analysis?
+# how do we deal with -? remove, make two words or ignore?
+#
 
