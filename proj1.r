d8f6682f38b9e55dc78d978ff3db665071fe6963
mhmc0808 <123744321+mhmc0808@users.noreply.github.com>
2025-09-30 14:45:10 +0100

    Update proj1.r

--- proj1.r
+++ proj1.r
@@ -1,22 +1,18 @@
-# GROUP WORK 1
+# Jackson Cramer (s2274544), Maximillian McCourt (s2145762), Natalia Montalvo Cabornero (s1969053)
 
-# Jackson Cramer, s2274544
-# Maximillian McCourt, s2145762
-# Natalia Montalvo Cabornero, s1969053
+# As a three, we collaborated on the development and optimisation of each exercise. While we divided forces, 
+# we ensured  a roughly equal distribution of work through code reviews, debugging sessions and meetings for problem solving. 
+# Jackson focused on question 4, and code optimisation/efficiency. Max focused on the functions in exercises 7,8 and 9, and Natalia
+# focused on exercises 5 and 6, and concise, clear code commenting.
 
-# List of tasks completed by each group member:
-# Jackson Cramer - primary focus on exercises 3 and 4.
-# Max McCourt - primary focus on exercises 7 and 8.
-# Natalia Montalvo Cabornero - primary focus on exercises 5 and 6.
-# We collaborative contributed on the development and optimization of each exercise. While we divided forces, 
-# we ensured  a roughly equal distribution of work trough code reviews, debugging sessions and meetings for problem solving. 
-
+# This code builds a model that uses the complete works of Shakespeare to generate sentences
+# that mimic the structure of Shakespearean writing.
 
 
 # Exercise 3
 
 # set working directory and read shakespeare text into R environment
-# setwd("GW1") ## comment out of submitted
+# setwd("GW1")
 a <- scan("shakespeare.txt",what="character",skip=83,nlines=196043-83,
           fileEncoding="UTF-8")
 
@@ -25,6 +21,7 @@ a <- scan("shakespeare.txt",what="character",skip=83,nlines=196043-83,
 
 # to remove stage directions (words located between square brackets), first locate all indices of opening brackets
 stage_start = grep("[",a,fixed=TRUE)
+# initialise empty vector to store indices of stage directions
 stage_dir <- c()
 # loop through each opening bracket
 for (left_bracket in stage_start){
@@ -39,28 +36,31 @@ for (left_bracket in stage_start){
 # remove all stage directions
 a <- a[-stage_dir]
 
-
 # remove character names (fully upper case) and arabic numerals, but do not remove I, A, or O
 a <- a[which(a == "I" | a == "A" | a == "O" | a != toupper(a))]
 
-# remove _ and - from words by replacing them by empty strings
+# remove _ and - from words by replacing them with empty strings
 a <- gsub("[_-]", "", a)
 
-# to seperate punctuation marks from words, create a split_punct function that takes in word vector w and vector of punctuation marks p
+# function that separates punctuation marks from words they are attached to
 split_punct <- function(w, p) {
-# for each punctuation p, place a space between the punctuation and connected word
+  # inputs:
+  #   w - word vector
+  #   p - punctuation marks vector
+  
+  # for each punctuation p, place a space between the punctuation and connected word
   for (punct in p) {
     w <- gsub(punct, paste(" ", punct, sep = ""), w, fixed=TRUE)
   }
-# put word vector into one string, with each item seperated by a space
+  # put word vector into one string, with each item separated by a space
   w <- paste(w, collapse = " ")
-# split punctuation and words by splitting on empty space, returning a vector of single words and single punctuation marks
+  # split punctuation and words by splitting on empty space, returning a vector of single words and single punctuation marks
   out <- strsplit(w, split = " ")[[1]]
   return(out)
 }
 
+# apply split_punct function to word vector a, using punctuation marks in p_to_use
 p_to_use <- c(",", ".", ";", "!", ":", "?")
-# apply split_punct function to word vector a using punctuation marks in p_to_use
 a <- split_punct(a, p_to_use)
 
 # convert word vector a to lower case
@@ -71,33 +71,32 @@ a <- tolower(a)
 
 # find all unique words
 unique_words <- unique(a)
-# index mapping each unique word to it's position in the text
+# index mapping each unique word to its position in the text
 word_idx <- match(a, unique_words)
 # tabulate function helps us to find the frequency of each unique word in the text
 word_freq <- tabulate(word_idx)
-# rank the words by frequency (the highest frequency is rank 1)
+# rank the words by frequency, the highest frequency being rank 1 (assigns words with same frequency the same average rank)
 freq_ranks <- rank(-word_freq, ties.method = "average")
-# get indices of top 1000 words
+# get indices of highest-ranking ~1000 words
 top_idx <- which(freq_ranks <= 1000)
-# extract the actual top 1000 words
+# extract ~1000 most-frequent unique words
 b <- unique_words[top_idx]
 
 
 # Exercise 6
 
-# index of the top words in the text, returning NA if not found
-b_idx <- match(a, b)
+# index of the most frequent words in the text (assigns NA if not in b)
+M1 <- match(a, b)
 
 # choose mlag, i.e. how many preceding words we are considering (here current word + 3 preceding)
 mlag <- 4 
 n <- length(a)
-
-# initialise M matrix with dimension (n-mlag) x (mlag+1)
+# To construct M matrix of common word token sequences, initialise empty matrix with dimension (n-mlag) x (mlag+1)
 M <- matrix(nrow=n-mlag, ncol=mlag+1)
-# fill the matrix: current word index in column 1 and then three lags in subsequent columns
+# fill the matrix with current word index in column 1, and then four lags in subsequent columns
 for (m in 1:(mlag+1)){
-  # slide through the text for each lag position
-  M[,m] <- b_idx[m:(n-(mlag+1)+m)]
+  # shift through the text for each lag position
+  M[,m] <- M1[m:(n-(mlag+1)+m)]
 }
 
 
@@ -107,8 +106,8 @@ for (m in 1:(mlag+1)){
 next.word <- function(key, M, M1, w=rep(1,ncol(M)-1)){
   # inputs:
   #   key - word sequence for which the next word is generated
-  #   M is matrix M with top word indexed and lags
-  #   M1 is the indexing vector of the entire text (index_vector)
+  #   M - matrix with indices of most frequent words and lags
+  #   M1 - indexing vector of the entire text (index_vector)
   #   w - vector of mixture weights
   
   # initialise next_word and match_rows
@@ -135,8 +134,11 @@ next.word <- function(key, M, M1, w=rep(1,ncol(M)-1)){
   random_row <- match_rows[sample(length(match_rows), 1)]
   # take next word from last entry of random_row
   next_word <- M[random_row, mlag+1]
+  print(b[next_word])
   return(next_word)
 }
+
+
 
 
 # Exercise 8 
