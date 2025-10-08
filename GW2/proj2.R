3b3204f34afb3178cae6b0fa8f5600bec4bf37d3
nataliamontalvo <N.Montalvo-Cabornero@sms.ed.ac.uk>
2025-10-08 17:59:23 +0100

    SEIR model proj2.R

--- GW2/proj2.R
+++ GW2/proj2.R
@@ -48,3 +48,32 @@ print(contacts)
 
 
 # can also use rbinom(1,1, prob=prob_beta_ij) instead of sampling
+
+
+
+
+# SEIR model 
+nseir <- function(beta,h,alink,alpha=c(.1,.01,.01),delta=.2,gamma=.4,nc=15, nt = 100,pinf = .005){
+  ## SEIR stochastic simulation model.
+  ## n = population size; ni = initially infective; nt = number of days
+  ## gamma = daily prob E -> I; delta = daily prob I -> R;
+  ## bmu = mean beta; bsc = var(beta) = bmu * bsc
+  
+  x <- rep(0,n) ## initialize to susceptible state
+  beta <- rgamma(n,shape=bmu/bsc,scale=bsc) ## individual infection rates
+  x[1:ni] <- 2 ## create some infectives
+  S <- E <- I <- R <- rep(0,nt) ## set up storage for pop in each state
+  S[1] <- n-ni;I[1] <- ni ## initialize
+  for (i in 2:nt) { ## loop over days
+    u <- runif(n) ## uniform random deviates
+    x[x==2&u<delta] <- 3 ## I -> R with prob delta
+    x[x==1&u<gamma] <- 2 ## E -> I with prob gamma
+    x[x==0&u<beta*I[i-1]] <- 1 ## S -> E with prob beta*I[i-1]
+    S[i] <- sum(x==0); E[i] <- sum(x==1)
+    I[i] <- sum(x==2); R[i] <- sum(x==3)
+  }
+  list(S=S,E=E,I=I,R=R,beta=beta)
+} 
+
+
+
